33d89a2d040259463b35b3341e9fb4630f5ed8ef
Paul Ericson <pericson12@gmail.com>
2023-09-15 10:40:51 -0400

    updated answers

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ document.getElementById("container");
 // 2. Select the section with an id of container using querySelector.
 document.querySelector("#container")
 // 3. Select all of the list items with a class of “second”.
+let footer = document.querySelector(".footer");
+footer.classList.add("main");
 document.querySelectorAll(".second")
 // 4. Select a list item with a class of third, but only the list item inside of the ol tag.
 document.querySelector("ol .third");
